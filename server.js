a3ea3b6b09e0e30851f74a72bab0c73f39a15baa
ranajunaid001 <130623056+ranajunaid001@users.noreply.github.com>
2025-11-24 07:06:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serve static files
 app.use(express.static('.'));
 
-// Serve index.html for all routes
+// Specific route for agent config
+app.get('/agent_config.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'agent_config.html'));
+});
+
+// Default route - serve index.html for root and any unmatched routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
